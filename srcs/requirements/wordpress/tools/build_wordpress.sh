bd25a9f3161121cb69248c539d0f4db66a125de5
Saad allah Samadi <ssamadi@e1r9p4.1337.ma>
2021-12-12 14:42:31 +0100

    wordpress+php

--- srcs/requirements/wordpress/tools/build_wordpress.sh
+++ srcs/requirements/wordpress/tools/build_wordpress.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 apt-get -y update
-# apt-get -y upgrade 
+apt-get -y upgrade 
 
-apt-get -y install php-fpm
+apt-get -y install php7.3-fpm
 apt-get -y update
 # apt-get -y upgrade
 
 apt-get -y install wget
 apt-get -y update
+
+#create /var/www/html
+cd /var
+mkdir /var/www
+mkdir /var/www/html
+service php7.3-fpm start
+service php7.3-fpm stop 
+cd /root
+mv conf/www.conf /etc/php/7.3/fpm/pool.d/.
 # apt-get -y upgrade 
 
